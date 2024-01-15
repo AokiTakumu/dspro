f817d6774ec0869d9f71fe42e8174b54552b0d38
AokiTakumu <s2322072@stu.musashino-u.ac.jp>
2024-01-15 23:37:28 +0900

    スクレイピング途中

--- .py
+++ .py
@@ -3,7 +3,7 @@ import requests
 import time
 
 # アクセスしたいWebサイトのURL
-url = 'https://www.data.jma.go.jp/stats/etrn/view/monthly_s1.php?prec_no=43&block_no=47626&year=2023&month=&day=&view='
+url = 'https://www.data.jma.go.jp/stats/etrn/view/daily_s1.php?prec_no=43&block_no=47626&year=2023&month=12&day=&view=a2'
 
 # Webサーバにリクエストを出す．レスポンスを変数に格納しておく
 r = requests.get(url)
@@ -11,6 +11,11 @@ r = requests.get(url)
 # 1秒待機
 time.sleep(1)
 
-soup = BeautifulSoup(r.content, "html.parser")
+html_soup = BeautifulSoup(r.content, "html.parser")
 
-print(soup.select("td"))
+# tdタグをすべて取得
+td_tags = html_soup.find_all('td', class_='data_0_0')
+
+# 取得したtdタグの内容を出力
+for td_tag in td_tags:
+    print(td_tag.text)
